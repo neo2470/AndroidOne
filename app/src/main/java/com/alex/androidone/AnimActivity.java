dfc461a0b8bc579af5b4239d0995c95ddbfc7967
Alex <zxfhacker@gmail.com>
2016-04-25 18:58:37 +0800

    add View Animation by Java Code

--- app/src/main/java/com/alex/androidone/AnimActivity.java
+++ app/src/main/java/com/alex/androidone/AnimActivity.java
@@ -10,23 +10,36 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.animation.AnticipateOvershootInterpolator;
 import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.alex.androidone.entity.FrameAnimationLoader;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * Created by alex on 16-4-1.
  * Android Animation
  */
 public class AnimActivity extends Activity implements FrameAnimationLoader.onCreateFrameAnimationListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,31 +49,22 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
         loadingView = (ImageView) findViewById(R.id.loadingAnim);
 
         ImageView foreAnim = (ImageView) findViewById(R.id.foreAnim);
-//        ImageView leftAnim = (ImageView) findViewById(R.id.leftAnim);
-//        ImageView rightAnim = (ImageView) findViewById(R.id.rightAnim);
-//        ImageView backAnim = (ImageView) findViewById(R.id.backAnim);
 
         new FrameAnimLoader(foreAnim).execute(0);
-//        new FrameAnimLoader(leftAnim).execute(1);
-//        new FrameAnimLoader(rightAnim).execute(2);
-//        new FrameAnimLoader(backAnim).execute(3);
 
         ImageView googleLoadingView = (ImageView) findViewById(R.id.googleLoadingAnim);
         FrameAnimationLoader animLoader = new FrameAnimationLoader(googleLoadingView, R.drawable.google_loading);
         animLoader.setOnCreateFrameAnimationListener(this);
         animLoader.start();
 
-        ImageView scaleView = (ImageView) findViewById(R.id.scaleView);
-        Animation scaleAnim = AnimationUtils.loadAnimation(this, R.anim.avator_set_in);
-        scaleView.startAnimation(scaleAnim);
-
-        TextView tranText = (TextView) findViewById(R.id.tranText);
-        Animation tranAnim = AnimationUtils.loadAnimation(this, R.anim.text_translate_in);
-        tranText.startAnimation(tranAnim);
+        imageView = (ImageView) findViewById(R.id.scaleView);
 
         objAnimView1 = (ImageView) findViewById(R.id.objAnimView1);
         objAnimView2 = (ImageView) findViewById(R.id.objAnimView2);
         objAnimView3 = (ImageView) findViewById(R.id.objAnimView3);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -68,7 +72,7 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
         super.onWindowFocusChanged(hasFocus);
 
         AnimationDrawable animDrawable = (AnimationDrawable) loadingView.getBackground();
-        if(hasFocus) {
+        if (hasFocus) {
             animDrawable.start();
         } else {
             animDrawable.stop();
@@ -90,8 +94,8 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
         final int ANIM_FRAME_COUNT = 32;
         final int ANIM_FRAME_INTERVAL = 70;
 
-        for(int i=0; i<ANIM_FRAME_COUNT; ++i) {
-            int x = i*ANIM_FRAME_WIDTH;
+        for (int i = 0; i < ANIM_FRAME_COUNT; ++i) {
+            int x = i * ANIM_FRAME_WIDTH;
             int y = 0;
             Drawable frame = new BitmapDrawable(Bitmap.createBitmap(bmp, x, y, ANIM_FRAME_WIDTH, ANIM_FRAME_HEIGHT));
             frameAnim.addFrame(frame, ANIM_FRAME_INTERVAL);
@@ -104,14 +108,14 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
 
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.objAnimBtn1 :
+            case R.id.objAnimBtn1:
                 ObjectAnimator objAnim1 = null;
                 int flag = 0;
-                if(null != view.getTag()) {
+                if (null != view.getTag()) {
                     flag = (int) view.getTag();
                 }
 
-                if(0 == (flag&1)) {
+                if (0 == (flag & 1)) {
                     objAnim1 = ObjectAnimator.ofFloat(objAnimView1, "translationY", -objAnimView1.getHeight());
                     flag = 1;
                 } else {
@@ -124,7 +128,7 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
                 objAnim1.start();
                 view.setTag(flag);
                 break;
-            case R.id.objAnimBtn2 :
+            case R.id.objAnimBtn2:
                 ValueAnimator valueObj = ObjectAnimator.ofInt(objAnimView2, "backgroundColor", 0xFFFF8080, 0xFF8080FF);
                 valueObj.setDuration(1000);
                 valueObj.setEvaluator(new ArgbEvaluator());
@@ -132,7 +136,7 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
                 valueObj.setRepeatMode(ValueAnimator.REVERSE);
                 valueObj.start();
                 break;
-            case R.id.objAnimBtn3 :
+            case R.id.objAnimBtn3:
                 ObjectAnimator objAnim3 = ObjectAnimator.ofFloat(objAnimView3, "rotation", 0, 360);
                 objAnim3.setDuration(3500);
                 objAnim3.setInterpolator(new LinearInterpolator());
@@ -140,13 +144,89 @@ public class AnimActivity extends Activity implements FrameAnimationLoader.onCre
                 objAnim3.setRepeatMode(ValueAnimator.RESTART);
                 objAnim3.start();
                 break;
+            case R.id.aAnim:// alpha animation
+                AlphaAnimation alphaAnim = new AlphaAnimation(0.1f, 1.0f);
+                alphaAnim.setDuration(1000);
+                alphaAnim.setRepeatCount(AlphaAnimation.INFINITE);
+                alphaAnim.setRepeatMode(AlphaAnimation.REVERSE);
+                alphaAnim.setInterpolator(new LinearInterpolator());
+                imageView.clearAnimation();
+                imageView.startAnimation(alphaAnim);
+                break;
+            case R.id.sAnim:// scale animation
+                ScaleAnimation scaleAnim = new ScaleAnimation(0.0f, 1.0f, 0.0f, 1.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnim.setDuration(500);
+                scaleAnim.setFillAfter(false);
+                scaleAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+                imageView.clearAnimation();
+                imageView.startAnimation(scaleAnim);
+                break;
+            case R.id.rAnim:// rotate animation
+                RotateAnimation rotateAnim = new RotateAnimation(0.0f, 360.f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotateAnim.setDuration(3000);
+                rotateAnim.setRepeatCount(Animation.INFINITE);
+                rotateAnim.setRepeatMode(Animation.RESTART);
+                rotateAnim.setInterpolator(new LinearInterpolator());
+                imageView.clearAnimation();
+                imageView.startAnimation(rotateAnim);
+                break;
+            case R.id.tAnim:// translate animation
+                TranslateAnimation tranAnim = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.5f, Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f);
+                tranAnim.setDuration(1000);
+                tranAnim.setInterpolator(new AnticipateOvershootInterpolator());
+                tranAnim.setRepeatMode(Animation.REVERSE);
+                tranAnim.setRepeatCount(Animation.INFINITE);
+                imageView.clearAnimation();
+                imageView.startAnimation(tranAnim);
+                break;
         }
     }
 
+    private ImageView imageView;
     private ImageView loadingView;
     private ImageView objAnimView1;
     private ImageView objAnimView2;
     private ImageView objAnimView3;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Anim Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.alex.androidone/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Anim Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.alex.androidone/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
 
 /**
